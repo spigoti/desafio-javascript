c176558679ad84167ae09e9d365e9db678f3a337
Felipe Spigoti <f.spigoti@gmail.com>
2020-04-01 01:32:39 +0000

    desafio 1 ok

--- desafio1.js
+++ desafio1.js
@@ -3,9 +3,15 @@ const name = "Anderson";
 exports.name = name;
 
 // 2. Declare uma nova variável chamada `soma`, adicione uma instrução somando os valores 15 e 8, e exporte ela como `sum`.
+const sum = 15 + 8;
+exports.sum = sum;
 
 // 3. Declare uma nova variável chamada `numbers` e atribua um array com os numeros de '1',  '5' e '10', e exporte ela como `numbers`.
+const numbers = [1, 5, 10];
+exports.numbers = numbers;
 
 // 4. Declare uma nova variável chamada `isBeautiful` atribua à ela o valor booleano que representa `verdadeiro`, e exporte ela como `isBeautiful`.
+const isBeautiful = true;
+exports.isBeautiful = isBeautiful;
 
 // 4. Declare uma nova variável chamada `isBeautiful` atribua à ela o valor booleano que representa `verdadeiro`, e exporte ela como `isBeautiful`.
